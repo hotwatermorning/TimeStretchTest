7d5e787b93b6e827a98caca7fba65ac1a0aee16a
hotwatermorning <hotwatermorning@gmail.com>
2018-12-03 00:53:17 +0900

    initial commit

---
+++ src/Buffer.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <vector>
+
+template<class T>
+class Buffer
+{
+public:
+    typedef T value_type;
+    Buffer()
+    :    channels_(0)
+    ,    samples_(0)
+    {}
+    
+    Buffer(size_t num_channels, size_t num_samples)
+    {
+        resize(num_channels, num_samples);
+    }
+    
+    size_t samples() const { return samples_; }
+    size_t channels() const { return channels_; }
+    
+    value_type ** data() { return buffer_heads_.data(); }
+    value_type const * const * data() const { return buffer_heads_.data(); }
+    
+    void resize(size_t num_channels, size_t num_samples)
+    {
+        std::vector<value_type> tmp(num_channels * num_samples);
+        std::vector<value_type *> tmp_heads(num_channels);
+        
+        channels_ = num_channels;
+        samples_ = num_samples;
+        
+        buffer_.swap(tmp);
+        buffer_heads_.swap(tmp_heads);
+        for(size_t i = 0; i < num_channels; ++i) {
+            buffer_heads_[i] = buffer_.data() + (i * num_samples);
+        }
+    }
+    
+    void fill(T value = T())
+    {
+        std::fill(buffer_.begin(), buffer_.end(), value);
+    }
+    
+    void resize_samples(size_t num_samples)
+    {
+        resize(channels(), num_samples);
+    }
+    
+    void resize_channels(size_t num_channels)
+    {
+        resize(num_channels, samples());
+    }
+    
+public:
+    std::vector<value_type> buffer_;
+    std::vector<value_type *> buffer_heads_;
+    
+    size_t channels_;
+    size_t samples_;
+};
+
+template<class T>
+class BufferRef
+{
+public:
+    BufferRef()
+    {
+        static T * dummy_ = nullptr;
+        data_ = &dummy_;
+        channels_ = 0;
+        samples_ = 0;
+    }
+    
+    template<class U>
+    struct get_data_type {
+        using type = typename std::conditional_t<std::is_const<U>::value, U const * const *, U**>;
+    };
+    
+    using data_type = typename get_data_type<T>::type;
+    using const_data_type = typename get_data_type<std::add_const_t<T>>::type;
+    
+    template<class U>
+    BufferRef(Buffer<U> &buffer) : BufferRef(buffer.data(), buffer.channels(), buffer.samples())
+    {}
+    
+    BufferRef(data_type data, int num_channels, int num_samples)
+    {
+        data_ = data;
+        channels_ = num_channels;
+        samples_ = num_samples;
+    }
+    
+    size_t samples() const { return samples_; }
+    size_t channels() const { return channels_; }
+    
+    data_type data() { return data_; }
+    const_data_type data() const { return data_; }
+    
+    void fill(T value = T())
+    {
+        for(int ch = 0; ch < channels_; ++ch) {
+            auto ch_data = data()[ch];
+            std::fill_n(ch_data, ch_data + samples_, value);
+        }
+    }
+    
+private:
+    data_type data_;
+    size_t channels_;
+    size_t samples_;
+};
---
+++ src/main.cpp
@@ -0,0 +1,201 @@
+#include <iostream>
+#include <RubberBandStretcher.h>
+#include <soundtouch/SoundTouch.h>
+#include <AudioFile.h>
+#include <cmath>
+#include <thread>
+#include "./Buffer.hpp"
+
+template<class Stretcher>
+Buffer<float> stretch(Buffer<float> const &src,
+                      double stretch_amount, double cent_change_amount,
+                      Stretcher &st
+                      );
+
+template<>
+Buffer<float> stretch(Buffer<float> const &src,
+                      double stretch_amount, double cent_change_amount,
+                      RubberBand::RubberBandStretcher &st
+                      )
+{
+    assert(stretch_amount >= 0);
+    
+    int const kProcessSize = 2048;
+    st.setMaxProcessSize(kProcessSize);
+    auto sent_change = pow(2.0, cent_change_amount / 1200.0);
+    st.setExpectedInputDuration(src.samples());
+    st.setPitchScale(sent_change);
+    st.setTimeRatio(stretch_amount);
+    
+    Buffer<float> dest(src.channels(), (int)(src.samples() * stretch_amount));
+    int dest_pos = 0;
+    
+    std::vector<float const *> src_heads(src.channels());
+    std::vector<float *> dest_heads(src.channels());
+
+    auto const length = src.samples();
+    
+    for(int src_pos = 0; src_pos < length; src_pos += kProcessSize) {
+        auto const num_to_send = std::min<int>(src_pos + kProcessSize, length) - src_pos;
+        
+        for(int ch = 0; ch < src.channels(); ++ch) {
+            src_heads[ch] = src.data()[ch] + src_pos;
+        }
+        
+        auto const finished = (src_pos + num_to_send == src.samples());
+        st.process(src_heads.data(), num_to_send, finished);
+
+        for( ; ; ) {
+            auto num_ready = st.available();
+            num_ready = std::min<int>(num_ready, kProcessSize);
+            
+            if(num_ready == -1) { return dest; } //< finished
+            
+            auto const num_to_receive = std::min<int>(dest_pos + num_ready, dest.samples()) - dest_pos;
+            if(num_to_receive == 0) {
+                //if(num_ready != 0) { return dest; }
+                break;
+            }
+            
+            for(int ch = 0; ch < src.channels(); ++ch) {
+                dest_heads[ch] = dest.data()[ch] + dest_pos;
+            }
+            
+            st.retrieve(dest_heads.data(), num_to_receive);
+            dest_pos += num_to_receive;
+        }
+    }
+    
+    assert(st.available() == -1);
+    return dest;
+}
+
+template<>
+Buffer<float> stretch(Buffer<float> const &src,
+                      double stretch_amount, double cent_change_amount,
+                      soundtouch::SoundTouch &st
+                      )
+{
+    assert(stretch_amount >= 0);
+    
+    int const kProcessSize = 2048;
+    st.setPitchSemiTones(cent_change_amount / 100.0);
+    st.setTempo(1 / stretch_amount);
+    
+    Buffer<float> dest(src.channels(), (int)(src.samples() * stretch_amount));
+    int dest_pos = 0;
+    
+    std::vector<float> src_interleaved(src.channels() * kProcessSize);
+    std::vector<float> dest_interleaved(src.channels() * kProcessSize);
+    
+    auto const length = src.samples();
+    
+    for(int src_pos = 0; src_pos < length; src_pos += kProcessSize) {
+        auto const num_send = std::min<int>(src_pos + kProcessSize, length) - src_pos;
+        
+        for(int ch = 0; ch < src.channels(); ++ch) {
+            for(int smp = 0; smp < num_send; ++smp) {
+                src_interleaved[smp * src.channels() + ch] = src.data()[ch][src_pos + smp];
+            }
+        }
+        
+        st.putSamples(src_interleaved.data(), num_send/* * src.channels()*/);
+        
+        auto const finished = (src_pos + num_send == src.samples());
+        if(finished) {
+            st.flush();
+        }
+
+        for( ; ; ) {
+            auto num_ready = st.numSamples()/* / src.channels() */;
+
+            num_ready = std::min<int>(num_ready, kProcessSize);
+            auto const num_receive = std::min<int>(dest_pos + num_ready, dest.samples()) - dest_pos;
+            if(num_receive == 0) { break; }
+
+            st.receiveSamples(dest_interleaved.data(), num_receive/* * src.channels() */);
+            
+            for(int ch = 0; ch < src.channels(); ++ch) {
+                for(int smp = 0; smp < num_receive; ++smp) {
+                    dest.data()[ch][dest_pos + smp] = dest_interleaved[smp * src.channels() + ch];
+                }
+            }
+            
+            dest_pos += num_receive;
+        }
+    }
+    
+    return dest;
+}
+
+enum class Library {
+    kSoundTouch,
+    kRubberband
+};
+
+//! @param ファイル名
+//! @param stretch_amount タイムストレッチ量。
+//!        2を指定すると、曲の長さが2倍になる（テンポは半分になる）。
+//!        0.5を指定すると、曲の長さが半分になる（テンポは2倍になる）。
+//! @param cent_change_amount ピッチシフト量。100を指定すると、100cent(i.e., 1半音)ピッチを上げる。
+//! @param library タイムストレッチ／ピッチシフトに使用するライブラリを指定する。
+void runner(std::string filename, double stretch_amount, double cent_change_amount, Library library)
+{
+    AudioFile<float> af_src;
+    
+    af_src.load(filename);
+    
+    Buffer<float> buf_src(af_src.getNumChannels(), af_src.getNumSamplesPerChannel());
+    for(int ch = 0; ch < buf_src.channels(); ++ch) {
+        std::copy(af_src.samples[ch].begin(),
+                  af_src.samples[ch].end(),
+                  buf_src.data()[ch]);
+    }
+    
+    auto call_stretch = [&](auto &st, double stretch_ratio, double cent_change_amount) {
+        return stretch(buf_src, stretch_ratio, cent_change_amount, st);
+    };
+    
+    Buffer<float> buf_dest;
+    if(library == Library::kSoundTouch) {
+        soundtouch::SoundTouch st;
+        st.setSampleRate(af_src.getSampleRate());
+        st.setChannels(af_src.getNumChannels());
+        buf_dest = call_stretch(st, 1.3, 700);
+    } else {
+        using RB = RubberBand::RubberBandStretcher;
+        
+        int const options = (RB::PercussiveOptions | RB::OptionProcessRealTime);
+        
+        RubberBand::RubberBandStretcher st(af_src.getSampleRate(),
+                                           af_src.getNumChannels(),
+                                           options);
+        
+        buf_dest = call_stretch(st, 1.3, 700);
+    }
+    
+    
+    AudioFile<float> af_dest;
+    af_dest.setAudioBufferSize(af_src.getNumChannels(), buf_dest.samples());
+    af_dest.setSampleRate(af_src.getSampleRate());
+    af_dest.setBitDepth(16);
+    
+    for(auto ch = 0; ch < af_src.getNumChannels(); ++ch) {
+        std::copy(buf_dest.data()[ch],
+                  buf_dest.data()[ch] + buf_dest.samples(),
+                  af_dest.samples[ch].begin());
+    }
+    
+    af_dest.save(filename + "_" + (library == Library::kSoundTouch ? "soundtouch" : "rubberband") + ".wav");
+}
+
+int main()
+{
+    double stretch_amount = 1.3;
+    double cent_change_amount = 500;
+    runner("../../resource/test.wav", stretch_amount, cent_change_amount, Library::kRubberband);
+    runner("../../resource/test.wav", stretch_amount, cent_change_amount, Library::kSoundTouch);
+    
+    std::cout << "finished." << std::endl;
+}
+
